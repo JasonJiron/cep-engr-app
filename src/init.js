a0698116ef6ba314c952e684cae9879e322be38d
Jason <jason.jiron@gmail.com>
2018-08-11 09:02:47 -0500

    Jason Jiron's About Me

--- src/init.js
+++ src/init.js
@@ -1,4 +1,36 @@
 // create a JavaScript object here with the following fields: firstName, lastName, jobTitle, homeOffice
 
-
+var aboutMe = {
+  firstName: "Jason",
+  lastName: "Jiron",
+  jobTitle: "Premier Partner / Traveler Care",
+  homeOffice: "RPP",
+  moreInfo: "I'm really excited to learn web/software development with HomeAway!"
+}
+console.log(aboutMe);
 // using jQuery and the object above, display the information as the appropriate button is clicked
+
+$(".btn1").click(function() {
+    $("#first-name").text(aboutMe.firstName);
+});
+
+$(".btn2").click(function() {
+  $("#last-name").text(aboutMe.lastName);
+});
+
+$(".btn3").click(function() {
+  $("#job-title").text(aboutMe.jobTitle);
+});
+
+$(".btn4").click(function() {
+  $("#home-office").text(aboutMe.homeOffice);
+});
+
+$(".btn5").click(function() {
+  $("#first-name").hide();
+  $("#last-name").hide();
+  $("#job-title").hide();
+  $("#home-office").hide();
+  $("#more-info").text(aboutMe.moreInfo);
+});
+
